cfcf517cb8153832710318029d37eec47e3486ee
rittmang <ritomgupta99@gmail.com>
2019-08-05 16:40:13 +0530

    Can view reviews of user

--- Cinewatchers.java
+++ Cinewatchers.java
@@ -1,4 +1,7 @@
 import javax.swing.*;
+import javax.swing.BorderFactory;
+import javax.swing.border.Border;
+
 import java.awt.*;
 import java.awt.event.*;
 import java.sql.Statement;
@@ -6,7 +9,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
- 
+ //add new user (Register) in SQL table: insert into users (username,password,name) values ('ritom','ritom','Ritom');
 public class Cinewatchers{
     String msg = "";
     JTextField txtUsername = null;
@@ -69,11 +72,14 @@ public class Cinewatchers{
                  {
                      if(password.equals(rs.getString(3)))
                      {
+                         String uid=rs.getString(1);//get uid
+                         String username=rs.getString(2);
+                         String name=rs.getString(4);
                          msg="Login Granted";
                          frame.getContentPane().removeAll();//removes all previous components, get a blank screen
                          frame.getContentPane().repaint();
                          Home h=new Home();
-                         h.go(frame);
+                         h.go(frame,uid,username,name);
                          signed_in=1;
                      }
                  } 
@@ -132,14 +138,105 @@ public class Cinewatchers{
 class Home
 {
     
-    public void go(JFrame frame)//uses same frame from Cinewatchers class
+    String user_id="",user_name="",name="";
+    JPanel p;JLabel rev;String all_reviews="";
+    JFrame new_frame;int already_shown=0;
+    public void go(JFrame frame,String uid,String username,String n)//uses same frame from Cinewatchers class
     {
+        user_id=uid;
+        user_name=username;
+        name=n;
+        new_frame=frame;
         JLabel l=new JLabel();
-        l.setText("Welcome to Cinewatchers");
-        JPanel p=new JPanel();
+        l.setText("Welcome to Cinewatchers, "+name);
+        l.setHorizontalAlignment(JLabel.CENTER);
+        p=new JPanel();
+        p.setLayout(new BoxLayout(p,BoxLayout.Y_AXIS));
         p.add(l);
-        frame.add(p);
-        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-        frame.setVisible(true);
+        new_frame.add(p);
+        new_frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        JButton btnShowReviews = new JButton("Show Reviews");
+        btnShowReviews.addActionListener(new ShowReviewsListener());
+        p.add(btnShowReviews);
+        //System.out.println(all_reviews);
+        //rev.setText(all_reviews);
+        //p.add(rev);
+        new_frame.getContentPane().add(BorderLayout.CENTER,p);
+        new_frame.setVisible(true);
+
     }
+    public class ShowReviewsListener implements ActionListener{
+        public void actionPerformed(ActionEvent event){
+            Statement stmt=null;
+            ResultSet rs=null;
+            if(already_shown==1)
+            {
+                JOptionPane.showMessageDialog(null,"All reviews done","Alert",JOptionPane.INFORMATION_MESSAGE);
+            }
+            else{
+            try{
+            Class.forName("com.mysql.cj.jdbc.Driver").newInstance(); 
+            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/cinewatchers","ritom","123123123");
+            stmt=con.createStatement();
+            rs=stmt.executeQuery("SELECT * FROM reviews WHERE uid="+user_id);
+            
+            rev=new JLabel();
+            
+
+            while(rs.next())
+            {
+                all_reviews+=rs.getString(3)+"\n\n";    
+            }
+            
+            rev.setText("<html>"+all_reviews.replaceAll("<","&lt;").replaceAll(">", "&gt;").replaceAll("\n","<br />")+"</html>");
+            rev.setHorizontalAlignment(JLabel.CENTER);
+            rev.setVerticalAlignment(JLabel.CENTER);
+            Border border = BorderFactory.createLineBorder(Color.BLACK);
+            rev.setBorder(border);
+            p.add(rev);
+            new_frame.validate();
+            new_frame.repaint();
+            already_shown=1;
+            con.close();
+            }
+            catch(SQLException e)
+            {
+                System.out.println("SQLException: " + e.getMessage());
+                System.out.println("SQLState: " + e.getSQLState());
+                System.out.println("VendorError: " + e.getErrorCode());
+            }
+            catch(Exception e)
+            {
+                System.out.println(e);
+            }
+            finally{
+                if(rs!=null)
+            {
+                try{
+                    rs.close();
+                }
+                catch(SQLException sqlEx)
+                {
+
+                }
+                rs=null;
+            }
+            if(stmt!=null)
+            {
+                try{
+                    stmt.close();
+                }
+                catch(SQLException sqlEx)
+                {
+
+                }
+                stmt=null;
+            }
+            }
+        }
+           // JOptionPane.showMessageDialog(null,msg,"Alert",JOptionPane.INFORMATION_MESSAGE);
+                              
+        }
+    }
+
 }
